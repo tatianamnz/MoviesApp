48f0de76c7d832607471c785b0eb9f9aef423603
Tatiana Muñoz <tatiana.munoz.romeo@gmail.com>
2022-11-01 18:24:23 +0100

    [APP] Añadir carousel

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import { ActivityIndicator, Button, Text, View } from 'react-native'
+import { ActivityIndicator, Button, Dimensions, Text, View } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 import { NavigationParams } from '../navigation/params'
 import { StackNavigationProp } from '@react-navigation/stack'
+import Carousel from 'react-native-snap-carousel'
 import { useMovies } from '../hooks/useMovies'
 import { MoviePoster } from '../components/MoviePoster'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
+
+const { width: windowWidth } = Dimensions.get('window')
 
 type HomeScreenProps = StackNavigationProp<NavigationParams, 'HomeScreen'>
 
@@ -30,8 +33,19 @@ export const HomeScreen = () => {
 
   return (
     <View style={{ marginTop: top + 20 }}>
-      <MoviePoster movie={cinemaMovies[9]} />
+      {/*<MoviePoster movie={cinemaMovies[9]} />*/}
       {/*<Button title="Ir detalle" onPress={goToDetails} />*/}
+      <View
+        style={{
+          height: 440
+        }}>
+        <Carousel
+          data={cinemaMovies}
+          renderItem={({ item }: any) => <MoviePoster movie={item} />}
+          sliderWidth={windowWidth}
+          itemWidth={300}
+        />
+      </View>
     </View>
   )
 }
